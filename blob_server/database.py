540fed176e5c7fc6c7760cb7bb66ef1d0dd421eb
madisonhicks <madison.carlisle.hicks@gmail.com>
2017-09-12 16:25:39 -0500

    docstrings

--- blob_server/database.py
+++ blob_server/database.py
@@ -12,17 +12,29 @@ from blob_server.config import (
 
 
 def db_uri():
+    """
+    creates url using defined environment variables
+    :return: database password
+    """
     return "postgresql://{}:{}@{}:{}/{}".format(
         SQL_USER, SQL_PASSWORD, SQL_DB_HOST, SQL_DB_PORT, SQL_DB_NAME
     )
 
 
 def get_engine():
+    """
+    creates the engine
+    :return: engine
+    """
     engine = create_engine(db_uri())
     return engine
 
 
 def get_session_registry(engine: Engine):
+    """
+    :param engine: engine created by get engine
+    :return: Session class
+    """
     session_factory = sessionmaker(bind=engine)
     Session = scoped_session(session_factory)
     return Session
