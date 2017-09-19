73b127dfd04fe662e48b16f0f64bf36e086a5687
madisonhicks <madison.carlisle.hicks@gmail.com>
2017-09-19 12:24:19 -0500

    add admin interface

--- blob_server/app.py
+++ blob_server/app.py
@@ -1,21 +1,34 @@
 from blob_server.database import get_session_registry, get_engine
 from blob_server.app_config import Config
+from blob_server.models import BlogPost, Tag
 from blob_server.resources import PostResource
 from flask import Flask
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_restful import Api
 
 
 def add_resources(app):
+    """adds /posts endpoint"""
     api = Api(app)
     api.add_resource(PostResource, '/posts')
 
 
+def add_admin_interface(app):
+    admin = Admin(app, name='madison', template_mode='bootstrap3')
+    admin.add_view(ModelView(BlogPost, app.session))
+    admin.add_view(ModelView(Tag, app.session))
+
+
 def create_app(config=Config):
+    """creates the app"""
     app = Flask(__name__)
     add_resources(app)
     app.engine = get_engine()
     app.session = get_session_registry(app.engine)
+    add_admin_interface(app)
     app.config.from_object(config)
     return app
+
 
 
