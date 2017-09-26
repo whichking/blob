3053aa05dd0eb2f27cdccf19306caa65fd3d2146
madisonhicks <madison.carlisle.hicks@gmail.com>
2017-09-26 12:32:21 -0500

    returns posts t

--- blob_server/app.py
+++ blob_server/app.py
@@ -1,11 +1,12 @@
-from blob_server.database import get_session_registry, get_engine
-from blob_server.app_config import Config
-from blob_server.models import BlogPost, Tag
-from blob_server.resources import PostResource
+from uuid import uuid4
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_restful import Api
+from blob_server.database import get_session_registry, get_engine
+from blob_server.app_config import Config
+from blob_server.models import BlogPost, Tag
+from blob_server.resources import PostResource
 
 
 def add_resources(app):
@@ -24,6 +25,7 @@ def create_app(config=Config):
     """creates the app"""
     app = Flask(__name__)
     add_resources(app)
+    app.secret_key = str(uuid4()).encode()
     app.engine = get_engine()
     app.session = get_session_registry(app.engine)
     add_admin_interface(app)
--- blob_server/resources.py
+++ blob_server/resources.py
@@ -1,7 +1,12 @@
 from flask import current_app as app
 from flask_restful import Resource
+from sqlalchemy import desc
+
+from blob_server.models import BlogPost
 
 
 class PostResource(Resource):
     def get(self):
-        return {'hello': 'world'}
+        query = app.session.query(BlogPost).order_by(desc(BlogPost.created))
+        posts = [post.serializable for post in query]
+        return posts
