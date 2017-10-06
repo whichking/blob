45d6023baa7be2282e391d12612f9c293fbff14f
madisonhicks <madison.carlisle.hicks@gmail.com>
2017-10-06 11:44:30 -0500

    async functions

---
+++ blob_client/src/api.js
@@ -0,0 +1,20 @@
+
+
+export async function getBlogPosts() {
+    const url = '/posts';
+    const resp = await fetch(
+        url,
+        {'headers': {'Content-Type': 'application/json'}},
+    );
+    return await resp.json();
+}
+
+
+export async function getBlogPost(id) {
+    const url = `/posts/${id}`;
+    const resp = await fetch(
+        url,
+        {'headers': {'Content-Type': 'application/json'}},
+    );
+    return await resp.json();
+}
