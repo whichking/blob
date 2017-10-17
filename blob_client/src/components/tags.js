36ea981f8cf922bd6b2d70dc6df306b5cd95298b
madisonhicks <madison.carlisle.hicks@gmail.com>
2017-10-17 11:07:56 -0500

    tags component

--- blob_client/src/components/tags.js
+++ blob_client/src/components/tags.js
@@ -8,3 +8,26 @@ export function Tag(props) {
             <a href={`/tags/${props.tag.id}`}>{props.tag.name}</a>
         </div>
     )
+}
+
+
+
+
+function Tags(props) {
+    const divStyle = {display: "flex"};
+    if (!("direction" in props) || (props.direction === "horizontal")) {
+        divStyle.flexFlow = "row wrap"
+    }
+    else if (props.direction === "vertical") {
+        divStyle.flexFlow = "column"
+    }
+    else {
+        throw "Invalid direction"
+    }
+
+    return(
+        <div style={divStyle}>
+            {props.tags.map((tag) => <Tag id={tag.id} name={tag.name}/>)}
+        </div>
+    )
+}
