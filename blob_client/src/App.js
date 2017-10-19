ce076539fff3d0178c7d3befa4bbf18035add083
madisonhicks <madison.carlisle.hicks@gmail.com>
2017-10-19 08:59:53 -0500

    tags component fix

--- blob_client/src/App.js
+++ blob_client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { getBlogPosts } from './api.js';
-import { Tag } from "./components/tags"
+import { Tags } from "./components/tags"
 
 
 class App extends Component {
@@ -24,7 +24,7 @@ class App extends Component {
           <div>
               {this.state.posts.map(post => <p>{JSON.stringify(post)}</p>)}
           </div>
-          <Tag tag={{'id': 1, 'name': 'great_tag'}}/>
+          <Tags tags={[{'id': 1, 'name': 'great_tag'}, {'id': 2, 'name': 'okay tag'}]} />
       </div>
     );
   }
--- blob_client/src/components/tags.js
+++ blob_client/src/components/tags.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 
 
-export function Tag(props) {
+function Tag(props) {
     return (
         <div>
             <a href={`/tags/${props.tag.id}`}>{props.tag.name}</a>
@@ -13,7 +13,7 @@ export function Tag(props) {
 
 
 
-function Tags(props) {
+export function Tags(props) {
     const divStyle = {display: "flex"};
     if (!("direction" in props) || (props.direction === "horizontal")) {
         divStyle.flexFlow = "row wrap"
@@ -27,7 +27,7 @@ function Tags(props) {
 
     return(
         <div style={divStyle}>
-            {props.tags.map((tag) => <Tag id={tag.id} name={tag.name}/>)}
+            {props.tags.map((tag) => <Tag tag={{'id':tag.id, 'name':tag.name}}/>)}
         </div>
     )
 }
