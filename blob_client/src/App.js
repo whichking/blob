14e3b40506b3eaceb88d2eb8388e9762ecf4cfd5
madisonhicks <madison.carlisle.hicks@gmail.com>
2017-10-19 14:59:36 -0500

    post component

--- blob_client/src/App.js
+++ blob_client/src/App.js
@@ -4,11 +4,20 @@ import ReactMarkdown from 'react-markdown';
 import './App.css';
 import { getBlogPosts } from './api.js';
 import { Tags } from "./components/tags"
-import { PostBody } from "./components/post"
+import { PostBody, Post } from "./components/post"
 
 
 let input = '# heading! \n\n**bold!** \n\n*italics!* \n\n![cow](http://www.healthbasics.net/wp-content/uploads/2015/07/cow-400x400.jpg "cow") \n\n```code block!```';
 
+let testPost = {
+    'content': 'foo',
+    'id': 1,
+    'created': '01-01-1900',
+    'tags': [
+        {'id': 1, 'name': 'a tag'},
+        {'id': 2, 'name': 'another tag'}
+    ]
+};
 
 class App extends Component {
     constructor(props) {
@@ -31,6 +40,7 @@ class App extends Component {
           <ReactMarkdown source={input} />
           <Tags tags={[{'id': 1, 'name': 'great_tag'}, {'id': 2, 'name': 'okay tag'}]} />
           <PostBody text='postbody test text'/>
+          <Post post={testPost} />
       </div>
     );
   }
